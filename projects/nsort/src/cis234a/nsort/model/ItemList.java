542becf87f13277e10d6db33c89636f6df147d0f
Chris Stultz <cstultz4@gmail.com>
2015-06-01 10:00:36 -0700

    removed unused HashMap

--- projects/nsort/src/cis234a/nsort/model/ItemList.java
+++ projects/nsort/src/cis234a/nsort/model/ItemList.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 public class ItemList
 {
     private ArrayList<Item> items;
-    private HashMap testImages = new HashMap();
 
     /**
      * Constructor for the class.
